4771653399c5dd2831374375ab7c2433bbcca783
timothyAgevi <timothyagevi@gmail.com>
2022-01-18 00:39:08 +0300

    add post method to data service

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { off } from 'process';
+import { Observable, of } from 'rxjs';
 import { UserSettings } from './user-settings';
 
 @Injectable({
@@ -7,7 +9,7 @@ import { UserSettings } from './user-settings';
 export class DataService {
 
   constructor() { }
-  postUserSettingsForm(userSettings:UserSettings){
-    
+  postUserSettingsForm(userSettings:UserSettings):Observable<UserSettings>{
+return of(userSettings);
   }
 }
