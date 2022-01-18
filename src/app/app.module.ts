cf69354cf76d468f32993cc408f4cfeacdad8903
timothyAgevi <timothyagevi@gmail.com>
2022-01-18 17:12:32 +0300

    install ngx bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
-import {HttpClientModule}from '@angular/common/http'
+import {HttpClientModule}from '@angular/common/http';
+import { ButtonsModule } from 'ngx-bootstrap/buttons'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserSettingsFormComponent,
-  
+    
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
